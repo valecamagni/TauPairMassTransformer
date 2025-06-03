13e35216ba3dc8ce143b537c68e026a061ea7058
Valentina Camagni <v.camagni1@campus.unimib.it>
2025-06-03 16:36:07 +0200

    submission script for fake taus selection

--- preprocessing/submit.py
+++ preprocessing/submit.py
@@ -1,7 +1,9 @@
 from optparse import OptionParser 
 import os 
 
-infiles = [os.getcwd()+'/DY_H_TT_SUSY140-350_semilep.txt']
+base_folder = '/gwdata/users/camagni/DiTau/TPMT_DATA/'
+
+infiles = [os.getcwd()+'/TTToSemiLeptonic.txt']
 
 files = [line.strip() for t in infiles for line in open(t) if not line.strip().startswith('#') and line.strip()]
 
@@ -11,28 +13,27 @@ for idx, file in enumerate(files):
 
     parts = file.split('/')
     sample = parts[-2]  
-    folder = sample
-
+    
     # Create the directories (if needed)
-    os.makedirs(folder, exist_ok=True)
-    os.makedirs(os.getcwd()+'/'+folder+'/JOBS', exist_ok=True)
-    os.makedirs(os.getcwd()+'/'+folder+'/DATA', exist_ok=True)
-    os.makedirs(os.getcwd()+'/'+folder+'/PLOTS', exist_ok=True)
-    os.makedirs(os.getcwd()+'/'+folder+'/LOGS', exist_ok=True)
+    os.makedirs(base_folder+sample, exist_ok=True)
+    os.makedirs(base_folder+sample+'/JOBS', exist_ok=True)
+    os.makedirs(base_folder+sample+'/DATA', exist_ok=True)
+    os.makedirs(base_folder+sample+'/PLOTS', exist_ok=True)
+    os.makedirs(base_folder+sample+'/LOGS', exist_ok=True)
 
-    outJobName =  os.getcwd()+'/'+folder+'/JOBS/job_' + str(idx) + '.sh'
-    inListName =  os.getcwd()+'/'+folder+'/JOBS/filelist_' + str(idx) + '.txt'
+    outJobName =  base_folder+sample+'/JOBS/job_' + str(idx) + '.sh'
+    inListName =  base_folder+sample+'/JOBS/filelist_' + str(idx) + '.txt'
     jobfilelist = open(inListName, 'w')
     jobfilelist.write(file+"\n")
     jobfilelist.close()
 
-    flat = int(1)
-    pairType2 = "mu_tau"
+    #command1 = "python3 " + "fully_hadronic.py" + " -i " + file + " -t false"
+    command2 = "python3 " + "semi_leptonic.py" + " -i " + file + " -t false" # default: ele_tau
+    command3 = "python3 " + "semi_leptonic.py" + " -i " + file + " -p mu_tau" + " -t false"
 
-    #command = "python3 " + "fully_hadronic.py" + " -i " + file + " -f " + str(flat) 
-    command1 = "python3 " + "semi_leptonic.py" + " -i " + file + " -f " + str(flat) # default: ele_tau
-    command2 = "python3 " + "semi_leptonic.py" + " -i " + file + " -f " + str(flat) + " -p " + pairType2
-
+    #command1 = "python3 " + "fully_hadronic.py" + " -i " + file 
+    #command2 = "python3 " + "semi_leptonic.py" + " -i " + file
+    #command3 = "python3 " + "semi_leptonic.py" + " -i " + file +  " -p mu_tau"
     
     scriptFile = open (outJobName, 'w')
     scriptFile.write ('#!/bin/bash\n') 
@@ -43,23 +44,24 @@ for idx, file in enumerate(files):
     scriptFile.write ('eosfusebind\n')
     scriptFile.write ('source ~gennai/.bashrc\n')
     scriptFile.write ('mamba activate torch\n')
-    scriptFile.write (command1+'\n')
+    #scriptFile.write (command1+'\n')
     scriptFile.write (command2+'\n')
+    scriptFile.write (command3+'\n')
     scriptFile.close()
 
     os.system ('chmod u+rwx ' + outJobName) 
 
-    condorFile = open ('%s/condorLauncher_%d.sh'% (os.getcwd()+'/'+folder+'/JOBS',idx), 'w')
+    condorFile = open ('%s/condorLauncher_%d.sh'% (base_folder+sample+'/JOBS',idx), 'w')
     condorFile.write ('Universe = vanilla\n')
     condorFile.write ('Executable  = '+outJobName +'\n')
     condorFile.write ('Log         = condor_job_$(ProcId).log\n')
     condorFile.write ('Output      = condor_job_$(ProcId).out\n')
     condorFile.write ('Error       = condor_job_$(ProcId).error\n')
     condorFile.write ('getenv       = True\n')
-    condorFile.write ('Requirements = (machine == "pccms02.hcms.it") || (machine == "pccms12.hcms.it") || (machine == "pccms13.hcms.it")\n')
+    condorFile.write ('Requirements = (machine == "pccms01.hcms.it") || (machine == "pccms02.hcms.it")\n')
     condorFile.write ('queue 1\n')
     condorFile.close ()
 
-    command = 'condor_submit '+ os.getcwd()+'/'+folder+'/JOBS' + '/condorLauncher_' + str(idx) + '.sh'
+    command = 'condor_submit '+ base_folder + sample +'/JOBS' + '/condorLauncher_' + str(idx) + '.sh'
     print(command)
     os.system (command) 
